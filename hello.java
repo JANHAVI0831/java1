6d85be766bdebb7422b770587151fa5e245feac5
JANHAVI0831 <t230160@famt.ac.in>
2025-10-27 09:45:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-
-
-public class HelloWorld {
+class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World");
     }
 }
